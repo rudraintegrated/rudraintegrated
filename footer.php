1dbb214b1943879370f408b309f03cc227c374d4
vaibhavsonwane <79414073+vaibhavsonwane@users.noreply.github.com>
2021-02-26 17:05:27 +0530

    Initial commit

---
+++ footer.php
@@ -0,0 +1,53 @@
+<div class="container-fluid">
+    <div class="jumbotron" style="background-color:#00264d; color: #fff;"> <h3 class="text-center"><b>CONTACT US</b></h3>
+  <form class="form-inline" action="/action_page.php">
+    <div class="form-group input-group">
+       <span class="input-group-addon" style="background-color:#00264d;"><i class="glyphicon glyphicon-user" style="color:#FBBC05;"></i></span>
+       <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+    </div>
+    <div class="form-group input-group">
+      <span class="input-group-addon" style="background-color:#00264d;"><i class="glyphicon glyphicon-envelope" style="color:#FBBC05;"></i></span>
+      <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+    </div>
+    <div class="form-group input-group">
+     <span class="input-group-addon" style="background-color:#00264d;"><i class="glyphicon glyphicon-earphone" style="color:#FBBC05;"></i></span>
+       <input class="form-control" id="mobile" name="mobile" placeholder="mobile" type="text" required maxlength="10" minlength="10">
+    </div>
+    <div class="form-group input-group">
+      <span class="input-group-addon" style="background-color:#00264d;"><i class="glyphicon glyphicon-comment" style="color:#FBBC05;"></i></span>
+       <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="1"></textarea>
+    </div>&nbsp;
+  
+    <button type="submit" class="btn btn-default btn-md">Submit</button>
+  </form>
+  <br>
+</div>
+</div>
+<br>
+<footer class="container-fluid text-muted" style="background-color: #000;">
+  
+  <p>Copyright &#169; 2018-2019 - All Rights Reserved - <a href="www.rudraintegrated.com">RUDRA INTEGRATED SERVICES LLP</a></p>
+</footer>
+
+<script language="JavaScript">
+<!-- 
+var space = " ";
+var speed = "150";
+var pos = 0;
+var msg = "Rudra Integrated Services LLP";
+function Scroll()
+{
+document.title = msg.substring(pos, msg.length) + space + msg.substring(0,pos);
+pos++;
+if (pos > msg.length) pos = 0;
+window.setTimeout("Scroll()", speed);
+}
+Scroll();
+-->
+</script>
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Rudra Integrated Services LLP</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.png">
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.gif">
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
+
+</head>
+<body>
+
+ </div>
+<?php
+include("mainmenu.php");
+?>
+
+<div class="container-fluid">
+    <div class="jumbotron" style="background-color:#fff; color: #00264d;">
+  <h4 class="text-center" style="color:#00264d;"><strong><sup><i class="fas fa-quote-left" style="font-size:18px; color:#ffcc00;"></i></sup> &nbsp;We know the road ahead is long, and exciting. We have many more milestones to establish.&nbsp;<sup><i class="fas fa-quote-right" style="font-size:18px;color:#ffcc00;"></i></sup></strong></h4>
+</div>
+
+</div>
+
+<div class="container-fluid">
+	 <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="images/original/con-og4.jpg" alt="" style="width:100%;">
+      </div>
+
+      <div class="item">
+        <img src="images/original/con-og2.jpg" alt="" style="width:100%;">
+      </div>
+    
+      <div class="item">
+        <img src="images/original/con-og7.jpg" alt="" style="width:100%;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+	
+</div>
+<br>
+<br>
+<div class="container-fluid" id="services">
+  <div class="jumbotron" style="background-color:#fff; color: #00264d;">
+    <h3 class="text-center"><b>SERVICES</b></h3>
+  <h4 class="text-center">OUR SERVICES ENABLE OWNERS ARCHITECTS, ENGINEERS,CONSULTANTS,CONTRACTORS AND BUILDERS TO PLAN, DESIGN AND BUILD THEIR PROJECTS IN THE MOST COMPETATIVE MANNERR BY  LEVERAGING OUR DOMAIN,TECHNOLOGY AND PROCESSES.</h4>
+</div>
+</div>
+
+<section>
+        	<div class="container-fluid">
+        		
+        		<div class="row">
+        			<div class="col-lg-4 col-md-6">
+        				<div class="jumbotron" style="background-color:#00264d; color: #fff;">
+        					<h4 class="text-center"><i class="fas fa-building" style="color:#FBBC05;font-size:36px"></i><br><b>BUILDERS & DEVELOPERS</b></h4>
+        					<h6 class="text-center">Building new horizon of civil sector with luxurious planning with the use of vastushastra.</h6>
+        				</div>
+        			</div>
+        			<div class="col-lg-4 col-md-6">
+        				<div class="jumbotron" style="background-color:#fff; color: #00264d;">
+        					<h4 class="text-center"><i class="fas fa-pencil-ruler" style="color:#000;font-size:36px"></i><br><b>ARCHITECTURAL  SERVICES</b></h4>
+        					<h6 class="text-center">covering every sector of civil industry,planning township appartments & different schemes.</h6>
+        				</div>
+        			</div>
+        			<div class="col-lg-4 col-md-6">
+        				<div class="jumbotron" style="background-color:#00264d; color: #fff;">
+        					<h4 class="text-center"><i class="fas fa-drafting-compass"style="color:#FBBC05;font-size:36px"></i><br><b>STRUCTURAL SERVICES</b></h4>
+        					<h6 class="text-center">Dealing the great experience in steel rebaring,PEB structures,RCC & steel design.</h6>
+        				</div>
+        			</div>
+        			<div class="col-lg-4 col-md-6">
+        				<div class="jumbotron" style="background-color:#fff; color: #00264d;">
+        					
+        					<h4 class="text-center"><i class="fas fa-briefcase" style="color:#000;font-size:36px"></i><br><b>SPECIAL  SERVICES</b></h4>
+        					<h6 class="text-center">The team of engineers serveing in sectors like valuation, surveys, Estimates in private & goverment.</h6>
+        				</div>
+        			</div>
+        			<div class="col-lg-4 col-md-6">
+        				<div class="jumbotron" style="background-color:#00264d; color: #fff;">
+        					
+        					<h4 class="text-center"><i class="fas fa-industry" style="color:#FBBC05;font-size:36px"></i><br><b>INFRASTRUCTURE</b></h4>
+        					<h6 class="text-center">Security in public sector for survey aspect like Bridges, Flyovers, Roads, Dams & Tunnels.</h6>
+        				</div>
+        			</div>
+        			<div class="col-lg-4 col-md-6">
+        				<div class="jumbotron" style="background-color:#fff; color: #00264d;">
+        					
+        					<h4 class="text-center"><i class="far fa-chart-bar" style="color:#000;font-size:36px"></i><br><b>PROJECT MANAGEMENT</b></h4>
+        					<h6 class="text-center">Using PERT CPM & Bar charts providing project managment services.</h6>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section>
+
+
+<div class="container-fluid">
+	
+	<div class="row">
+  		<div class="col-xs-6 col-sm-8 col-lg-10">
+  			
+  		
+  		</div>
+  		<div class="col-xs-6 col-sm-4 col-lg-2">
+  			 
+  		</div>
+	</div>
+</div>
+
+
+
+<div class="container-fluid">
+  <div class="jumbotron" style="background-color:#fff; color: #00264d;">
+  <h4 class="text-center">We dared to dream big. A dream to consulting not just for homes, but lifestyles. A dream to create integrated workspaces, Our approach is focused on planned development, creating value assets for the mankind, and to become the leader in our spheres of business.</h4>
+</div>
+</div>
+
+
+<?php
+include("footer.php");
+?>
+
+</body>
+</html>
+
+
